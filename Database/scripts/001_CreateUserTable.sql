88b59f5acbfd8f917d588f947d9037008a5f508b
sathindu <sathindu.d.zoysa@gmail.com>
2026-02-21 00:53:31 +0530

    RP-3 CI/CD Pipeline Implementation Fix the database issue

--- Database/scripts/001_CreateUserTable.sql
+++ Database/scripts/001_CreateUserTable.sql
@@ -1,16 +1,3 @@
--- Switch to or create the database
-IF NOT EXISTS (SELECT name
-FROM master.sys.databases
-WHERE name = 'restroplate')
-BEGIN
-  CREATE DATABASE restroplate;
-END
-GO
-
--- Use the database
-USE restroplate;
-GO
-
 -- Drop table if exists
 IF OBJECT_ID('dbo.users', 'U') IS NOT NULL
     DROP TABLE dbo.users;
@@ -20,25 +7,19 @@ GO
 CREATE TABLE dbo.users
 (
   user_id INT IDENTITY(1,1) NOT NULL,
-  -- AUTO_INCREMENT equivalent
+
   email NVARCHAR(255) NOT NULL,
   password_hash NVARCHAR(255) NOT NULL,
   phone_number NVARCHAR(20) NULL,
 
-  -- ENUM replacement using CHECK constraint
   user_type NVARCHAR(50) NOT NULL
     CHECK (user_type IN ('DONOR', 'DISTRIBUTION_CENTER')),
 
-  -- JSON stored as NVARCHAR(MAX) in SQL Server
   address NVARCHAR(MAX) NULL,
 
-  -- TIMESTAMP default CURRENT_TIMESTAMP
   created_at DATETIME NOT NULL DEFAULT GETDATE(),
 
   CONSTRAINT PK_users PRIMARY KEY (user_id),
   CONSTRAINT UQ_users_email UNIQUE (email)
 );
 GO
-
--- Table is empty in your dump, so no data insertion needed
--- If there were data, we would convert MySQL INSERT statements to T-SQL INSERT statements
