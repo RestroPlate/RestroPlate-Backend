4ab5ea16643b88895f78c97b80a42face7542874
sathindu <sathindu.d.zoysa@gmail.com>
2026-02-20 23:32:08 +0530

    RP-3 CI/CD Pipeline Implementation

--- Controllers/Program.cs
+++ Controllers/Program.cs
@@ -1,8 +1,15 @@
 using RestroPlate.Models.Interfaces;
 using RestroPlate.Repository.Database;
 using RestroPlate.Repository;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Load .env file in development
+if (builder.Environment.IsDevelopment())
+{
+    Env.Load();
+}
 
 // Register services.
 builder.Services.AddControllers();
